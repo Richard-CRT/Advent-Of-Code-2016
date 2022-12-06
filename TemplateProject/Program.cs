c3d30617795fe4859b452f0b23f4e94d86c890fb
Richard-CRT <richard-crt@users.noreply.github.com>
2022-12-06 10:12:40 +0000

    Initial commit

---
+++ TemplateProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCodeUtilities;
+
+List<string> inputList = AoCUtilities.GetInputLines();
+
+void P1()
+{
+    int result = 0;
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+
+void P2()
+{
+    int result = 0;
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+
+P1();
+P2();
